a59eaddd5835a6991aa5c6ce02f7eaf05f506a0b
Rowan Molony <rowan.molony@codema.ie>
2021-06-23 16:17:59 +0100

    Remove BER Public from GUI
    Gooey couldn't handle passing a filepath via a widget to
    a radio group checkbox - seems designed for a mere True | False
    selection.  As a workaround will have a separate GUI specifically
    for BER Public

--- ibsg/gui.py
+++ ibsg/gui.py
@@ -2,29 +2,22 @@ from gooey import Gooey
 from gooey import GooeyParser
 
 
-@Gooey(program_name="ibsg")
-def gui():
-    parser = GooeyParser(
-        description="Generate a csv file of individual Irish buildings"
-    )
-    chosen_ber_dataset = parser.add_mutually_exclusive_group(
-        required=True, gooey_options={"initial_selection": 0}
-    )
-    chosen_ber_dataset.add_argument(
-        "--private-ber",
-        dest="BER Public",
-        action="store_true",
-        help="Download the Open-Access BER Public Dataset",
-    )
-    chosen_ber_dataset.add_argument(
-        "--public-ber",
-        dest="BER Private",
-        action="store_true",
-        help="Upload the Closed-Access Small Area Extract of the BER Public Dataset",
+@Gooey(program_name="ibsg", timing_options={"show_time_remaining": True})
+def ber_private():
+    description = "Generate a csv file of individual Irish buildings"
+    parser = GooeyParser(description=description)
+    parser.add_argument(
+        dest="InputFile",
         widget="FileChooser",
+        help="Upload BER Small Area Extract CSV File",
     )
-    parser.parse_args()
+    parser.add_argument(
+        dest="OutputDirectory",
+        widget="DirChooser",
+        help="Save Directory for Processed Data",
+    )
+    return parser.parse_args()
 
 
 if __name__ == "__main__":
-    config = gui()
+    ber_private()
