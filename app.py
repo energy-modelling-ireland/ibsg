95b136eff0aa2909c0ffef04bbf81bfc6c43e153
Rowan Molony <rowan.molony@codema.ie>
2021-07-01 11:55:08 +0100

    Apply cleaning filters on form submit
    Only clean the data once all filters have been selected & refactor
    the filters into one function per column filter

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import datetime
+import pathlib
 from typing import List
 
 import pandas as pd
@@ -7,31 +9,63 @@ from ibsg import clean
 from ibsg import POSTCODES
 from ibsg import COUNTIES
 
+OUTPUT_COLUMNS = [
+    "small_area",
+    "year_of_construction",
+    "energy_value",
+    "type_of_rating",
+    "living_area_percent",
+    "roof_area",
+    "roof_uvalue",
+    "wall_area",
+    "wall_uvalue",
+    "floor_area",
+    "floor_uvalue",
+    "window_area",
+    "window_uvalue",
+    "door_area",
+    "door_uvalue",
+    "ground_floor_area",
+    "first_floor_area",
+    "second_floor_area",
+    "third_floor_area",
+    "main_sh_boiler_efficiency",
+    "main_hw_boiler_efficiency",
+    "main_sh_boiler_efficiency_adjustment_factor",
+    "main_hw_boiler_efficiency_adjustment_factor",
+    "suppl_sh_boiler_efficiency_adjustment_factor",
+]
+
 
 def main():
+    st.header(" 🧹 Irish Building Energy Ratings (BER)")
     ## Load
     sa_bers_file = st.file_uploader(label="Upload Small Area BERs", type=["csv"])
-    raw_sa_bers = _load_small_area_bers(sa_bers_file)
+    raw_sa_bers = _load_small_area_bers(sa_bers_file, columns=OUTPUT_COLUMNS)
     sa_ids_2016 = _load_small_area_ids()
 
-    ## Clean
-    clean_small_area_bers = _clean_small_area_bers(
-        bers=raw_sa_bers, small_area_ids=sa_ids_2016
-    )
+    with st.form("Apply Filters"):
+        ## Filter
+        sa_bers_in_counties = _filter_by_county(raw_sa_bers, COUNTIES)
+        sa_bers_in_postcodes = _filter_by_postcode(raw_sa_bers, POSTCODES)
+        filtered_bers = min([sa_bers_in_counties, sa_bers_in_postcodes], key=len)
+        submit_button = st.form_submit_button(label="Apply")
 
-    ## Filter
-    sa_bers_in_counties = _filter_by_county(clean_small_area_bers, COUNTIES)
-    sa_bers_in_postcodes = _filter_by_county(clean_small_area_bers, POSTCODES)
-    filtered_bers = min([sa_bers_in_counties, sa_bers_in_postcodes], key=len)
+    if submit_button:
+        ## Clean
+        clean_small_area_bers = _clean_small_area_bers(
+            bers=raw_sa_bers, small_area_ids=sa_ids_2016
+        )
 
 
 @st.cache
-def _load_small_area_bers(file) -> pd.DataFrame:
+def _load_small_area_bers(file, columns: List[str]) -> pd.DataFrame:
     if file:
-        bers = pd.read_csv(file.getvalue())
+        bers = pd.read_csv(file)
     else:
         bers = pd.read_csv("data/BER.public.14.05.2021/BER.public.14.05.2021.csv")
-    return bers
+    standardised_bers = bers.pipe(clean.standardise_ber_private_column_names)
+    return standardised_bers[columns]
 
 
 @st.cache
@@ -41,23 +75,37 @@ def _load_small_area_ids() -> List[str]:
 
 @st.cache
 def _clean_small_area_bers(
-    bers: pd.DataFrame, small_area_ids: List[str]
+    bers: pd.DataFrame,
+    small_area_ids: List[str],
 ) -> pd.DataFrame:
     return (
-        bers.pipe(clean.standardise_ber_private_column_names)
-        .pipe(clean.is_not_provisional)
-        .pipe(clean.is_realistic_floor_area)
-        .pipe(clean.is_realistic_living_area_percentage)
-        .pipe(clean.is_realistic_boiler_efficiency)
-        .pipe(clean.is_realistic_boiler_efficiency_adjustment_factor)
-        .pipe(clean.is_realistic_boiler_efficiency)
-        .pipe(clean.is_real_small_area, small_area_ids)
+        bers.pipe(clean.is_not_provisional)
+        .pipe(clean.is_valid_floor_area)
+        .pipe(clean.is_valid_living_area_percentage)
+        .pipe(clean.is_valid_sh_boiler_efficiency)
+        .pipe(clean.is_valid_hw_boiler_efficiency)
+        .pipe(clean.is_valid_sh_boiler_efficiency_adjustment_factor)
+        .pipe(clean.is_valid_hw_boiler_efficiency_adjustment_factor)
+        .pipe(clean.is_valid_suppl_boiler_efficiency_adjustment_factor)
+        .pipe(clean.is_valid_small_area_id, small_area_ids)
     )
 
 
 def _raise_for_all_query(x):
     if "All" in x:
         raise ValueError("Please deselect 'All'!")
+
+
+def _filter_by_columns(df: pd.DataFrame, columns: List[str]) -> pd.DataFrame:
+    selected_columns = st.multiselect(
+        "Select Columns", ["All"] + columns, default="All"
+    )
+    if selected_columns == ["All"]:
+        selected_rows = df[columns]
+    else:
+        _raise_for_all_query(selected_columns)
+        selected_rows = df[selected_columns]
+    return selected_rows
 
 
 def _filter_by_county(bers: pd.DataFrame, counties: List[str]) -> pd.DataFrame:
@@ -77,7 +125,7 @@ def _filter_by_county(bers: pd.DataFrame, counties: List[str]) -> pd.DataFrame:
 
 def _filter_by_postcode(bers: pd.DataFrame, postcodes: List[str]) -> pd.DataFrame:
     selected_postcodes = st.multiselect(
-        "Select Counties", ["All"] + postcodes, default="All"
+        "Select Postcodes", ["All"] + postcodes, default="All"
     )
     if selected_postcodes == ["All"]:
         selected_bers = bers
--- ibsg/clean.py
+++ ibsg/clean.py
@@ -1,5 +1,6 @@
 import json
 from typing import Dict
+from typing import List
 from typing import Optional
 
 import pandas as pd
@@ -9,7 +10,7 @@ from ibsg import DEFAULTS
 
 def standardise_ber_private_column_names(
     ber: pd.DataFrame,
-    mappings: Optional[Dict[str, str]] = DEFAULTS["mappings"]["BER Private"],
+    mappings: Optional[Dict[str, str]] = DEFAULTS["mappings"]["small_area_bers"],
 ):
     return ber.rename(columns=mappings)
 
@@ -18,25 +19,43 @@ def is_not_provisional(ber: pd.DataFrame) -> pd.DataFrame:
     return ber.query("`type_of_rating` != 'Provisional    '")
 
 
-def is_realistic_floor_area(ber: pd.DataFrame) -> pd.DataFrame:
+def is_valid_floor_area(ber: pd.DataFrame) -> pd.DataFrame:
     return ber.query("`ground_floor_area` > 30 and `ground_floor_area` < 1000")
 
 
-def is_realistic_living_area_percentage(ber: pd.DataFrame) -> pd.DataFrame:
+def is_valid_living_area_percentage(ber: pd.DataFrame) -> pd.DataFrame:
     return ber.query("`living_area_percent` < 90 or `living_area_percent` > 5")
 
 
-def is_realistic_boiler_efficiency(ber: pd.DataFrame) -> pd.DataFrame:
+def is_valid_sh_boiler_efficiency(ber: pd.DataFrame) -> pd.DataFrame:
+    return ber.query("main_sh_boiler_efficiency > 19")
+
+
+def is_valid_hw_boiler_efficiency(ber: pd.DataFrame) -> pd.DataFrame:
     return ber.query(
-        "`main_sh_boiler_efficiency` > 19"
-        " or `main_hw_boiler_efficiency` < 320"
-        " or `main_hw_boiler_efficiency` > 19"
+        "main_hw_boiler_efficiency < 320 or main_hw_boiler_efficiency > 19"
     )
 
 
-def is_realistic_boiler_efficiency_adjustment_factor(ber: pd.DataFrame) -> pd.DataFrame:
-    return ber.query(
-        "`main_sh_boiler_efficiency_adjustment_factor` > 0.7"
-        " and `main_hw_boiler_efficiency_adjustment_factor` > 0.7"
-        " and `suppl_sh_boiler_efficiency_adjustment_factor` > 19"
-    )
+def is_valid_sh_boiler_efficiency_adjustment_factor(
+    ber: pd.DataFrame,
+) -> pd.DataFrame:
+    return ber.query("main_sh_boiler_efficiency_adjustment_factor > 0.7")
+
+
+def is_valid_hw_boiler_efficiency_adjustment_factor(
+    ber: pd.DataFrame,
+) -> pd.DataFrame:
+    return ber.query("main_hw_boiler_efficiency_adjustment_factor > 0.7")
+
+
+def is_valid_suppl_boiler_efficiency_adjustment_factor(
+    ber: pd.DataFrame,
+) -> pd.DataFrame:
+    return ber.query("suppl_sh_boiler_efficiency_adjustment_factor > 19")
+
+
+def is_valid_small_area_id(
+    ber: pd.DataFrame, small_area_ids: List[str]
+) -> pd.DataFrame:
+    return ber.query("small_area in @small_area_ids")
