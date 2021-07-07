a92ac943da6a0692d5f25fcf89678ca5527f2940
Rowan Molony <rowan.molony@codema.ie>
2021-07-07 12:49:35 +0100

    Create archetypes on columns using average values
    Enables archetype creation of average uvalues (etc) for each dwelling
    type in stock or if have good data for each dwelling type in each
    location

--- ibsg/archetype.py
+++ ibsg/archetype.py
@@ -9,10 +9,19 @@ import pandas as pd
 Operations = Dict[str, Union[str, Callable]]
 
 
+def _get_mode_or_first_occurence(s: pd.Series) -> str:
+    mode = s.mode()
+    if len(mode) > 1:
+        most_common = mode[0]
+    else:
+        most_common = mode
+    return most_common
+
+
 def _get_aggregation_operations(df):
     numeric_operations = {c: "median" for c in df.select_dtypes("number").columns}
     categorical_operations = {
-        c: pd.Series.mode
+        c: _get_mode_or_first_occurence
         for c in set(
             df.select_dtypes("object").columns.tolist()
             + df.select_dtypes("string").columns.tolist()
@@ -20,3 +29,40 @@ def _get_aggregation_operations(df):
         )
     }
     return {**numeric_operations, **categorical_operations}
+
+
+def create_archetypes(
+    stock: pd.DataFrame,
+    on_columns: List[str],
+    aggregation_operations: Optional[Operations] = None,
+    sample_size: int = 30,
+) -> pd.DataFrame:
+    """Create archetypes on stock of significant sample_size.
+
+    Args:
+        stock (pd.DataFrame): Building Stock DataFrame
+        on_columns (List[str]): Column names to archetype on
+        aggregation_operations (Optional[Operations], optional): Column names mapped to
+            aggregation operations (mode, median, mean). Defaults to None.
+        sample_size (int, optional): Sample size above which archetyping is considered.
+            Defaults to 30.
+
+    Returns:
+        pd.DataFrame: Archetypes DataFrame
+    """
+    archetype_group_sizes = stock.groupby(on_columns).size().rename("sample_size")
+    if aggregation_operations:
+        use_columns = on_columns + list(aggregation_operations.keys())
+    else:
+        use_columns = stock.columns
+        agg_columns = list(set(use_columns).difference(on_columns))
+        aggregation_operations = _get_aggregation_operations(stock[agg_columns])
+
+    return (
+        stock.loc[:, use_columns]
+        .groupby(on_columns)
+        .agg(aggregation_operations)
+        .join(archetype_group_sizes)
+        .query(f"sample_size > {sample_size}")
+        .reset_index()
+    )
--- tests/test_archetype.py
+++ tests/test_archetype.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from pandas.testing import assert_frame_equal
 
 from ibsg import archetype
 
@@ -14,9 +15,9 @@ def test_get_aggregation_operations():
         }
     )
     expected_output = {
-        "cat": pd.Series.mode,
-        "str": pd.Series.mode,
-        "obj": pd.Series.mode,
+        "cat": archetype._get_mode_or_first_occurence,
+        "str": archetype._get_mode_or_first_occurence,
+        "obj": archetype._get_mode_or_first_occurence,
         "int": "median",
         "float": "median",
     }
@@ -24,3 +25,66 @@ def test_get_aggregation_operations():
     output = archetype._get_aggregation_operations(df)
 
     assert output == expected_output
+
+
+def test_create_archetypes():
+    stock = pd.DataFrame(
+        {
+            "dwelling_type": [
+                "Detached house",
+                "Mid-terrace house",
+                "End of terrace house",
+                "Detached house",
+                "Detached house",
+                "End of terrace house",
+                "Detached house",
+                "Detached house",
+                "Ground-floor apartment",
+                "Ground-floor apartment",
+            ],
+            "wall_uvalue": [
+                1.83,
+                2.09,
+                0.60,
+                1.77,
+                0.28,
+                0.37,
+                0.30,
+                2.00,
+                0.50,
+                0.94,
+            ],
+            "main_sh_boiler_fuel": [
+                "Heating Oil                   ",
+                "Mains Gas                     ",
+                "Heating Oil                   ",
+                "Heating Oil                   ",
+                "Heating Oil                   ",
+                "Mains Gas                     ",
+                "Heating Oil                   ",
+                "Heating Oil                   ",
+                "Electricity                   ",
+                "Mains Gas                     ",
+            ],
+        }
+    )
+    expected_output = pd.DataFrame(
+        {
+            "dwelling_type": [
+                "Detached house",
+                "End of terrace house",
+                "Ground-floor apartment",
+            ],
+            "wall_uvalue": [1.77, 0.485, 0.72],
+            "main_sh_boiler_fuel": [
+                "Heating Oil                   ",
+                "Heating Oil                   ",
+                "Electricity                   ",
+            ],
+            "sample_size": [5, 2, 2],
+        }
+    )
+    output = archetype.create_archetypes(
+        stock=stock, on_columns=["dwelling_type"], sample_size=1
+    )
+    assert_frame_equal(output, expected_output)
