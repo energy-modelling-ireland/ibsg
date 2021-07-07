288ad34382e11d0d314c04fa2b1505460601525e
Rowan Molony <rowan.molony@codema.ie>
2021-07-07 18:38:36 +0100

    Add archetype & census link options to app.py

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from pathlib import Path
 import pandas as pd
 import streamlit as st
 
+from ibsg import archetype
+from ibsg import census
 from ibsg import DEFAULTS
 from ibsg import postcodes
 from ibsg import small_areas
@@ -31,10 +33,8 @@ def main():
     with st.beta_expander("Hints"):
         st.markdown(
             f"""
-        - You might need to install [7zip](https://www.7-zip.org/) for:
-            - Compressing your closed-access small area BER dataset to a `zip` file of
-            under 200MB
-            - Open the `csv.gz` files 
+        - You might need to install [7zip](https://www.7-zip.org/) to compress your
+        closed-access small area BER dataset to a `zip` file under 200MB
         - `ibsg` won't be able to read your zipped Small Area BERs `csv` file if the
         column names don't match:
 
@@ -43,34 +43,59 @@ def main():
         )
 
     c1, c2 = st.beta_columns(2)
-    postcode_bers_selected = c1.button("Fetch Postcode BERs")
+    postcode_bers_is_selected = c1.button("Fetch Postcode BERs")
     small_area_bers_zipfile = c2.file_uploader(
         "Upload Small Area BERs",
         type="zip",
     )
+    small_area_bers_is_selected = bool(small_area_bers_zipfile)
+    census_is_selected = st.checkbox("Link to the 2016 census?", value=True)
+    archetype_is_selected = st.checkbox(
+        "Fill unknown buildings with archetypes?", value=True
+    )
 
-    if small_area_bers_zipfile:
+    if small_area_bers_is_selected:
         small_area_bers = small_areas.main(small_area_bers_zipfile)
-        create_csv_download_link(
-            small_area_bers, f"small_area_bers_{datetime.date.today()}"
+        census_bers, census_is_selected = census.main(
+            small_area_bers, archetype_is_selected
         )
-    if postcode_bers_selected:
+        archetyped_bers, archetypes = archetype.main(
+            census_bers, archetype_is_selected, census_is_selected
+        )
+        create_csv_download_link(
+            archetyped_bers,
+            filename=f"small_area_bers_{datetime.date.today()}",
+            suffix=".csv.zip",
+        )
+        if archetypes:
+            for name, data in archetypes.items():
+                create_csv_download_link(
+                    data,
+                    filename=f"{name}_archetypes_{datetime.date.today()}",
+                    suffix=".csv",
+                )
+    elif postcode_bers_is_selected:
+        st.info("'Link to 2016 census?' not yet implemented!")
+        st.info("'Fill unknown buildings with archetypes?' not yet implemented!")
         postcode_bers = postcodes.main()
         create_csv_download_link(
-            postcode_bers, f"postcode_bers_{datetime.date.today()}"
+            postcode_bers,
+            filename=f"postcode_bers_{datetime.date.today()}",
+            suffix=".csv.gz",
+            compression="gzip",
         )
 
 
-def create_csv_download_link(df: pd.DataFrame, filename: str):
+def create_csv_download_link(df: pd.DataFrame, filename: str, suffix: str):
     # workaround from streamlit/streamlit#400
-    st.markdown("Saving data as a `csv.gz` file...")
-    STREAMLIT_STATIC_PATH = Path(st.__path__[0]) / "static"
-    DOWNLOADS_PATH = STREAMLIT_STATIC_PATH / "downloads"
-    if not DOWNLOADS_PATH.is_dir():
-        DOWNLOADS_PATH.mkdir()
-    filepath = (DOWNLOADS_PATH / filename).with_suffix(".csv.gz")
-    df.to_csv(filepath, index=False, compression="gzip")
-    st.markdown(f"[{filepath.name}](downloads/{filepath.name})")
+    with st.spinner(f"Saving '{filename}{suffix}' to disk..."):
+        STREAMLIT_STATIC_PATH = Path(st.__path__[0]) / "static"
+        DOWNLOADS_PATH = STREAMLIT_STATIC_PATH / "downloads"
+        if not DOWNLOADS_PATH.is_dir():
+            DOWNLOADS_PATH.mkdir()
+        filepath = (DOWNLOADS_PATH / filename).with_suffix(suffix)
+        df.to_csv(filepath, index=False)
+        st.markdown(f"[{filepath.name}](downloads/{filepath.name})")
 
 
 if __name__ == "__main__":
