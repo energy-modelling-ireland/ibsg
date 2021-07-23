83d76a13373409fe349197802a0bb8b4a8d03ee0
Rowan Molony <rowan.molony@codema.ie>
2021-07-23 15:20:01 +0100

    Filter SA & Pcode BERs via same function
    Had to adapt the clean filters to accomodate the fact that the SA BERs
    do not contain all columns referenced by the EMNI filters by raising
    a warning when called but doing nothing!  Unfortunately this injects a
    streamlit dependency into ibsg/

--- ibsg/main.py
+++ ibsg/main.py
@@ -1,21 +1,18 @@
 from configparser import ConfigParser
 from io import BytesIO
 from pathlib import Path
-from typing import Any
+from typing import Any, List
 from typing import Dict
-from zipfile import ZipFile
 
-import icontract
 import fsspec
 import pandas as pd
 import streamlit as st
 
-from ibsg import bers
+from ibsg import clean
 from ibsg import CONFIG
 from ibsg import _DATA_DIR
 from ibsg import DEFAULTS
 from ibsg import io
-from ibsg import postcodes
 
 
 def generate_building_stock(
@@ -30,22 +27,112 @@ def generate_building_stock(
             dtype=defaults["small_areas"]["dtype"],
             mappings=defaults["small_areas"]["mappings"],
             filepath=data_dir / config["small_area_bers"]["filename"],
+        ).pipe(
+            _filter_bers,
+            selected_filters=selections["filters"],
+            bounds=selections["bounds"],
         )
     else:
-        with fsspec.open(
+        fs = fsspec.filesystem(config["postcode_bers"]["filesystem"])
+        with fs.open(
             config["postcode_bers"]["url"],
-            filecache={"cache_storage", data_dir / config["postcode_bers"]["filename"]},
+            cache_storage=data_dir / config["postcode_bers"]["filename"],
         ) as f:
-            bers = pd.read_parquet(f)
+            bers = pd.read_parquet(f).pipe(
+                _filter_bers,
+                selected_filters=selections["filters"],
+                bounds=selections["bounds"],
+            )
 
 
 def _load_small_area_bers(
     zipped_csv: BytesIO, dtype: Dict[str, Any], mappings: Dict[str, Any], filepath: Path
 ) -> pd.DataFrame:
-    breakpoint()
     if not filepath.exists():
         io.convert_zipped_csv_to_parquet(
             zipped_csv=zipped_csv, dtype=dtype, mappings=mappings, filepath=filepath
         )
         del zipped_csv
     return pd.read_parquet(filepath)
+
+
+def _filter_bers(
+    bers: pd.DataFrame, selected_filters: List[str], bounds: Dict[str, Dict[str, int]]
+) -> pd.DataFrame:
+    return (
+        bers.pipe(
+            clean.get_rows_meeting_condition,
+            filter_name="Is not provisional",
+            selected_filters=selected_filters,
+            condition="type_of_rating != 'Provisional    '",
+        )
+        .pipe(
+            clean.get_rows_meeting_condition,
+            filter_name="lb < ground_floor_area < ub",
+            selected_filters=selected_filters,
+            condition="ground_floor_area > {lb} and ground_floor_area < {ub}".format(
+                lb=bounds["ground_floor_area"]["lb"],
+                ub=bounds["ground_floor_area"]["ub"],
+            ),
+        )
+        .pipe(
+            clean.get_rows_meeting_condition,
+            filter_name="lb < living_area_percent < ub",
+            selected_filters=selected_filters,
+            condition="living_area_percent > {lb} or living_area_percent < {ub}".format(
+                lb=bounds["living_area_percent"]["lb"],
+                ub=bounds["living_area_percent"]["ub"],
+            ),
+        )
+        .pipe(
+            clean.get_rows_meeting_condition,
+            filter_name="lb < main_sh_boiler_efficiency < ub",
+            selected_filters=selected_filters,
+            condition="main_sh_boiler_efficiency > {lb} or main_sh_boiler_efficiency < {ub}".format(
+                lb=bounds["main_sh_boiler_efficiency"]["lb"],
+                ub=bounds["main_sh_boiler_efficiency"]["ub"],
+            ),
+        )
+        .pipe(
+            clean.get_rows_meeting_condition,
+            filter_name="lb < main_hw_boiler_efficiency < ub",
+            selected_filters=selected_filters,
+            condition="main_hw_boiler_efficiency > {lb} or main_hw_boiler_efficiency < {ub}".format(
+                lb=bounds["main_hw_boiler_efficiency"]["lb"],
+                ub=bounds["main_hw_boiler_efficiency"]["ub"],
+            ),
+        )
+        .pipe(
+            clean.get_rows_meeting_condition,
+            filter_name="main_sh_boiler_efficiency_adjustment_factor > lb",
+            selected_filters=selected_filters,
+            condition="main_sh_boiler_efficiency_adjustment_factor > {lb}".format(
+                lb=bounds["main_sh_boiler_efficiency_adjustment_factor"]["lb"],
+            ),
+        )
+        .pipe(
+            clean.get_rows_meeting_condition,
+            filter_name="main_hw_boiler_efficiency_adjustment_factor > lb",
+            selected_filters=selected_filters,
+            condition="main_hw_boiler_efficiency_adjustment_factor > {lb}".format(
+                lb=bounds["main_hw_boiler_efficiency_adjustment_factor"]["lb"],
+            ),
+        )
+        .pipe(
+            clean.get_rows_meeting_condition,
+            filter_name="declared_loss_factor < ub",
+            selected_filters=selected_filters,
+            condition="declared_loss_factor < {ub}".format(
+                ub=bounds["declared_loss_factor"]["ub"],
+            ),
+        )
+        .pipe(
+            clean.get_rows_meeting_condition,
+            filter_name="lb < thermal_bridging_factor < ub",
+            selected_filters=selected_filters,
+            condition="thermal_bridging_factor > {lb} or thermal_bridging_factor <= {ub}".format(
+                lb=bounds["thermal_bridging_factor"]["lb"],
+                ub=bounds["thermal_bridging_factor"]["ub"],
+            ),
+        )
+    )
