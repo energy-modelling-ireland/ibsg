b1a1922db3b98adafa531e419c41fd3325d78e4a
Rowan Molony <rowan.molony@codema.ie>
2021-07-23 13:55:09 +0100

    Migrate BER Filters to homepage
    After this refactor all UI components will be in app.py and all logic in
    ibsg/ - this is necessary as a separation of interests and even could
    allow streamlit to be replaced with some other UI framework.  The
    immediate goal is for the logic to function like a CLI with all inputs
    specified upfront thus enabling deterministic runs that do not
    pass DataFrame's between functions thus keeping under memory limits
    for streamlit

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
+from collections import defaultdict
 from configparser import ConfigParser
 import datetime
 from pathlib import Path
 from typing import Any
 from typing import Dict
+from typing import List
+from typing import Tuple
 
 import pandas as pd
 import streamlit as st
@@ -99,13 +102,86 @@ def main(
         archetype, else do 'small area | period built', finally do 'period built'
         """,
     )
+    selections["filters"], selections["bounds"] = _select_ber_filters()
     selections["download_filetype"] = st.selectbox(
         "Download format?",
         options=[".csv.gz", ".parquet"],
         help="You might need to install 7zip to unzip '.csv.gz' (see hints)",
     )
-
     _generate_building_stock(selections=selections, data_dir=data_dir, config=config)
+
+
+def _select_ber_filters() -> Tuple[List[str], Dict[str, Dict[str, int]]]:
+    filter_names = [
+        "Is not provisional",
+        "lb < ground_floor_area < ub",
+        "lb < living_area_percent < ub",
+        "lb < main_sh_boiler_efficiency < ub",
+        "lb < main_hw_boiler_efficiency < ub",
+        "main_sh_boiler_efficiency_adjustment_factor > lb",
+        "main_hw_boiler_efficiency_adjustment_factor > lb",
+        "declared_loss_factor < ub",
+        "lb < thermal_bridging_factor < ub",
+    ]
+    selected_filters: List[str] = st.multiselect(
+        "Select Filters",
+        options=filter_names,
+        default=filter_names,
+        help="lb = Lower Bound, ub = Upper Bound",
+    )
+
+    with st.beta_expander("Change BER Filter Bounds?"):
+        c1, c2 = st.beta_columns(2)
+        return selected_filters, {
+            "ground_floor_area": {
+                "lb": c1.number_input("Lower Bound: ground_floor_area", value=0),
+                "ub": c2.number_input("Upper Bound: ground_floor_area", value=1000),
+            },
+            "living_area_percent": {
+                "lb": c1.number_input("Lower Bound: living_area_percent", value=5),
+                "ub": c2.number_input("Lower Bound: living_area_percent", value=90),
+            },
+            "main_sh_boiler_efficiency": {
+                "lb": c1.number_input(
+                    "Lower Bound: main_sh_boiler_efficiency", value=19
+                ),
+                "ub": c2.number_input(
+                    "Lower Bound: main_sh_boiler_efficiency", value=600
+                ),
+            },
+            "main_hw_boiler_efficiency": {
+                "lb": c1.number_input(
+                    "Lower Bound: main_hw_boiler_efficiency", value=19
+                ),
+                "ub": c2.number_input(
+                    "Lower Bound: main_hw_boiler_efficiency", value=320
+                ),
+            },
+            "main_sh_boiler_efficiency_adjustment_factor": {
+                "lb": st.number_input(
+                    "Lower Bound: main_sh_boiler_efficiency_adjustment_factor",
+                    value=0.7,
+                ),
+            },
+            "main_hw_boiler_efficiency_adjustment_factor": {
+                "lb": st.number_input(
+                    "Lower Bound: main_hw_boiler_efficiency_adjustment_factor",
+                    value=0.7,
+                ),
+            },
+            "declared_loss_factor": {
+                "ub": st.number_input(
+                    "Upper Bound: declared_loss_factor",
+                    value=20,
+                ),
+            },
+            "thermal_bridging_factor": {
+                "lb": c1.number_input("Lower Bound: thermal_bridging_factor", value=0),
+                "ub": c2.number_input(
+                    "Lower Bound: thermal_bridging_factor", value=0.15
+                ),
+            },
+        }
 
 
 def _get_bers(selections: Dict[str, Any], config: ConfigParser):
@@ -136,7 +212,7 @@ def _generate_building_stock(
                         bers, selections=selections, config=config
                     )
                 with st.spinner("Filling unknown census buildings with archetypes..."):
-                    archetyped_bers, archetypes = archetype.main(
+                    archetyped_bers = archetype.main(
                         census_bers, selections=selections, config=config
                     )
                 selected_bers = archetyped_bers
