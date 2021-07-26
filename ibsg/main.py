2fb954036bf4aeceaabb2620f0ea7503be56dd0e
Rowan Molony <rowan.molony@codema.ie>
2021-07-26 14:56:36 +0100

    Archetype unknown buildings
    Restore archetyping to this branch until solve the resource limit
    problem to enable archetyping on local resources.

--- ibsg/main.py
+++ ibsg/main.py
@@ -34,17 +34,37 @@ def generate_building_stock(
                 filepath=data_dir / config["postcode_bers"]["filename"],
             ).pipe(ber.filter_bers, selections=selections, defaults=defaults)
 
-    with st.spinner("Linking BERs to the 2016 Census..."):
-        if selections["census"] & selections["archetype"]:
-            st.error(
-                """
-                Cannot fill the Census with BER Archetypes on the streamlit sharing
-                free-tier resources!  Please deselect 'Fill Unknown Buildings with 
-                Archetypes?' and try again
-                """
+    if selections["census"] & selections["archetype"]:
+        with st.spinner("Linking BERs to the 2016 Census & Filling with Archetypes..."):
+            buildings = (
+                census.load_census_buildings(
+                    url=config["census_buildings"]["url"],
+                    filepath=data_dir / config["census_buildings"]["filename"],
+                )
+                .pipe(
+                    clean.get_rows_containing_substrings,
+                    column_name="countyname",
+                    selected_substrings=selections["countyname"],
+                    all_substrings=defaults["countyname"],
+                )
+                .pipe(census.replace_not_stated_period_built_with_mode)
+                .pipe(
+                    merge.fill_census_with_bers,
+                    bers=ber_buildings,
+                    ber_granularity=selections["ber_granularity"],
+                )
+                .pipe(
+                    archetype.fillna_with_archetypes,
+                    archetype_columns=[
+                        [selections["ber_granularity"], "period_built"],
+                        ["period_built"],
+                    ],
+                    sample_size=config["settings"]["sample_size"],
+                )
             )
-            return
-        elif selections["census"]:
+            del ber_buildings
+    elif selections["census"]:
+        with st.spinner("Linking BERs to the 2016 Census..."):
             buildings = (
                 census.load_census_buildings(
                     url=config["census_buildings"]["url"],
@@ -64,8 +84,8 @@ def generate_building_stock(
                 )
             )
             del ber_buildings
-        else:
-            buildings = ber_buildings
+    else:
+        buildings = ber_buildings
 
     with st.spinner("Saving data..."):
         if ".gz" == filepath.suffix:
