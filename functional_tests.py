c67b8b0ef71c91cc3f4fbf91ca6e1cd6b4fbc170
Rowan Molony <rowan.molony@mainstreamrp.com>
2021-12-12 11:46:22 +0000

    Test main function in FT
    Enables dropping breakpoints into end to end runs and debugging
    intermediate steps

--- functional_tests.py
+++ functional_tests.py
@@ -12,7 +12,6 @@ from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 import app
-import globals
 
 
 @pytest.fixture
@@ -26,36 +25,37 @@ def browser() -> webdriver.Remote:
 
 
 @pytest.fixture
-def zipped_bers(tmp_path: Path) -> BytesIO:
+def sample_bers(tmp_path: Path) -> BytesIO:
     bers = pd.read_csv("sample-BERPublicsearch.txt", sep="\t")
-    content = bers.to_csv(index=False, sep="\t")
-    file = BytesIO()
-    with ZipFile(file, "w") as zf:
-        zf.writestr("BERPublicsearch.txt", content)
-    return file.getvalue()
+    f = bers.to_csv(index=False, sep="\t")
+    filepath = tmp_path / "BERPublicsearch.zip"
+    with ZipFile(filepath, "w") as zf:
+        zf.writestr("BERPublicsearch.txt", f)
+    return ZipFile(filepath).read("BERPublicsearch.txt")
 
 
 @responses.activate
 def test_user_can_download_default_bers(
     browser: webdriver.Remote,
-    zipped_bers: BytesIO,
+    sample_bers: BytesIO,
     monkeypatch: MonkeyPatch,
     tmp_path: Path,
 ) -> None:
-    defaults = app.get_defaults()
+    defaults = app._get_defaults()
     responses.add(
         responses.POST,
         defaults["download"]["url"],
-        body=zipped_bers,
+        body=sample_bers,
         content_type="application/x-zip-compressed",
         headers={
             "content-disposition": "attachment; filename=BERPublicSearch.zip"
         },
         status=200,
     )
-    monkeypatch.setattr(globals, "get_streamlit_download_dir", lambda: tmp_path)
-    monkeypatch.setattr(globals, "get_data_dir", lambda: tmp_path)
+    monkeypatch.setattr(app, "_get_streamlit_download_dir", lambda: tmp_path)
+    monkeypatch.setattr(app, "_get_data_dir", lambda: tmp_path)
     expected_output = tmp_path / "BERPublicsearch.csv.gz"
+
     browser.set_window_size(1024, 768)
 
     # Bob opens the website
@@ -64,12 +64,11 @@ def test_user_can_download_default_bers(
     ## Wait 3s for the selenium browser to load the application
     sleep(3)
 
-    # Clicks download
-    download_button = browser.find_element_by_xpath('//button[text()="Download?"]')
-    download_button.click()
+    # Scrolls down & is delighted to see filters & a download button!
+    browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
-    ## Force browser to scroll down to download button
-    browser.execute_script("arguments[0].scrollIntoView();", download_button)
+    # Clicks download
+    browser.find_element_by_xpath('//button[text()="Download?"]').click()
 
     # The filtered BERs appear in his downloads folder
     assert expected_output.exists()
